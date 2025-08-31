0c561fdb6e589dd7f4925dfa43cd0ac4a69e0f2f
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2025-08-31 16:11:19 -0300

    ajuste de layout para ocupaçaõ total da página

--- src/components/QLayout.vue
+++ src/components/QLayout.vue
@@ -6,29 +6,29 @@ import QRow from "./QRow.vue";
 </script>
 
 <template>
-<div class="min-h-screen flex flex-col">
-  <div class="border-b">
-    <header class="max-w-screen-2xl mx-auto w-full">
-      <slot name="header"></slot>
-    </header>
+  <div class="min-h-screen flex flex-col">
+    <div class="border-b">
+      <header class="max-w-screen-2xl mx-auto w-full">
+        <slot name="header"></slot>
+      </header>
+    </div>
+
+    <div class="max-w-screen-2xl mx-auto w-full flex-1">
+      <QRow class="flex">
+        <template #sidebar>
+          <aside class="w-[220px] pr-8 border-r py-6 h-full hidden lg:block">
+            <slot name="sidebar"></slot>
+          </aside>
+        </template>
+
+        <main class="py-6">
+          <slot name="main-content"></slot>
+        </main>
+      </QRow>
+    </div>
+
+    <footer class="border-t">
+      <slot name="footer"></slot>
+    </footer>
   </div>
-
-  <div class="max-w-screen-2xl mx-auto w-full">
-    <QRow class="flex">
-      <template #sidebar>
-        <aside class="w-[220px] pr-8 border-r py-6 h-full hidden lg:block">
-          <slot name="sidebar"></slot>
-        </aside>
-      </template>
-
-      <main class="py-6">
-        <slot name="main-content"></slot>
-      </main>
-    </QRow>
-  </div>
-
-  <footer class="border-t">
-    <slot name="footer"></slot>
-  </footer>
-</div>
 </template>
